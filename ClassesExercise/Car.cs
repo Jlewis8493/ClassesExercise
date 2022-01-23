eec88e59c5c652c17c27f0634bb6d1c8f2bbc358
Jared Lewis <jlewis8493@gmail.com>
2022-01-23 14:50:48 -0500

    Exercise 1 complete, build successful

---
+++ ClassesExercise/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace ClassesExercise
+{
+    public class Car
+    {
+        public Car()
+        {
+
+        }
+
+        public Car(string makeInput, string modelInput, int year)
+        {
+            Make = makeInput;
+            Model = modelInput;
+            Year = year;
+        }
+
+
+        //Create a make property of type string that is public
+        public string Make { get; set; } //get = read set = write
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
